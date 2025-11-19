3059a3852322b154bf3d1a56f0887363bddad9c4
Ayan Pandey <aayanpandey8528@gmail.com>
2025-11-19 15:04:23 +0530

    Add overflow-hidden to clip icons within 75% width region

--- Portfolio/src/components/tech_stack.jsx
+++ Portfolio/src/components/tech_stack.jsx
@@ -50,15 +50,15 @@ const Tech_stack = () => {
   const duplicatedStack = [...techStack, ...techStack];
 
   return (
-    <div className="relative w-full bg-black py-10 overflow-hidden flex justify-center">
-      {/* Scrolling Container with left/right space */}
-      <div className="relative w-3/4 mx-auto">
+    <div className="relative w-full bg-black py-10 overflow-hidden">
+      {/* Scrolling Container */}
+      <div className="relative w-3/4 mx-auto overflow-hidden">
         {/* Gradient Overlays for smooth fade */}
         <div className="absolute left-0 top-0 bottom-0 w-32 bg-gradient-to-r from-black to-transparent z-10"></div>
         <div className="absolute right-0 top-0 bottom-0 w-32 bg-gradient-to-l from-black to-transparent z-10"></div>
 
-        {/* Scrolling Track with side margins */}
-        <div className="flex gap-16 animate-scroll-fast px-16 md:px-32 lg:px-40">
+        {/* Scrolling Track */}
+        <div className="flex gap-16 animate-scroll-fast">
           {duplicatedStack.map((tech, index) => {
             const Icon = tech.icon;
             return (
