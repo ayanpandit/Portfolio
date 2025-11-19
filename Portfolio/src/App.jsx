a54248dc401e5f4bac455b19f63a56b82cf97b05
Ayan Pandey <aayanpandey8528@gmail.com>
2025-11-19 15:19:40 +0530

    called inthe app.jsx

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/hero";
 import Tech_stack from "./components/tech_stack";
 import About from "./components/about";
+import Services from "./components/services";
 function App()
 {
   return (
@@ -8,6 +9,7 @@ function App()
       <Hero />
       <Tech_stack />
       <About />
+      <Services />
     </>
   );
 }
